ba2ca329b3613cf6d38e7241954cda0428475533
Powerack <53368926+Powerack@users.noreply.github.com>
2023-12-03 09:54:15 +0100

    Update content.js

--- AukroBlocker/content.js
+++ AukroBlocker/content.js
@@ -1,38 +1,42 @@
-chrome.storage.sync.get("blockEnabled", function (data) {
-  var blockEnabled = data.blockEnabled !== undefined ? data.blockEnabled : true;
+chrome.storage.sync.get(["blockEnabled", "blockedUsers", "blockedRegex"], function (data) {
+    var blockEnabled = data.blockEnabled !== undefined ? data.blockEnabled : true;
 
-  if (window.location.hostname === "aukro.cz" && blockEnabled) {
-    chrome.storage.sync.get("blockedUsers", function (data) {
-      var blockedUsers = data.blockedUsers || [];
+    if (window.location.hostname === "aukro.cz" && blockEnabled) {
+        var blockedUsers = data.blockedUsers || [];
+        var blockedRegexList = data.blockedRegex || [];
 
-      function findAndHideBlockedOffers() {
-        blockedUsers.forEach(function (username) {
-          var offerCards = document.querySelectorAll("auk-advanced-item-card");
-          offerCards.forEach(function (offerCard) {
-            var usernameElement = offerCard.querySelector(".small-user-name");
-            if (usernameElement && usernameElement.textContent.trim() === username) {
-              offerCard.style.display = "none";
+        function findAndHideBlockedOffers() {
+            blockedUsers.forEach(function (username) {
+                var offerCards = document.querySelectorAll("auk-advanced-item-card");
+                offerCards.forEach(function (offerCard) {
+                    var usernameElement = offerCard.querySelector(".small-user-name");
+                    if (usernameElement && usernameElement.textContent.trim() === username) {
+                        offerCard.style.display = "none";
+                    }
+                });
+            });
+
+            blockedRegexList.forEach(function (regex) {
+                var offerCards = document.querySelectorAll("auk-advanced-item-card");
+                offerCards.forEach(function (offerCard) {
+                    var titleElement = offerCard.querySelector("auk-basic-item-card-title");
+                    if (titleElement && titleElement.textContent.match(new RegExp(regex, 'i'))) {
+                        offerCard.style.display = "none";
+                    }
+                });
+            });
+        }
+
+        findAndHideBlockedOffers();
+
+        var observer = new MutationObserver(function (mutationsList) {
+            for (var mutation of mutationsList) {
+                if (mutation.type === "childList") {
+                    findAndHideBlockedOffers();
+                }
             }
-          });
         });
 
-        var productScrollSlider = document.querySelector("advanced-item-card");
-        if (productScrollSlider) {
-          productScrollSlider.style.display = "none";
-        }
-      }
-
-      findAndHideBlockedOffers();
-
-      var observer = new MutationObserver(function (mutationsList) {
-        for (var mutation of mutationsList) {
-          if (mutation.type === "childList") {
-            findAndHideBlockedOffers();
-          }
-        }
-      });
-
-      observer.observe(document.body, { childList: true, subtree: true });
-    });
-  }
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
 });
